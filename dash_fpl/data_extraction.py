a47cc58a52c56977f9003557de57a7115bd7a40e
Brendan <brendanpatrickbaker7@gmail.com>
2022-03-04 14:02:38 +0000

    Single Linechart

--- dash_fpl/data_extraction.py
+++ dash_fpl/data_extraction.py
@@ -1,21 +1,73 @@
 import requests, json
 from pprint import pprint
 import pandas as pd
+
 # base url for all FPL API endpoints
 
-
+base_url = 'https://fantasy.premierleague.com/api/'
 # get data from bootstrap-static endpoint
+pd.set_option('display.max_columns', 500)
 
 
 # show the top level fields
-#pprint(r, indent=2, depth=1, compact=True)
+# pprint(r, indent=2, depth=1, compact=True)
 
 # get player data from 'elements' field
 # show data for first player
-#players = pd.json_normalize(r['elements'])
+# players = pd.json_normalize(r['elements'])
 
 def getPlayer():
     base_url = 'https://fantasy.premierleague.com/api/'
-    r = requests.get(base_url+'bootstrap-static/').json()
+    r = requests.get(base_url + 'bootstrap-static/').json()
     players = pd.json_normalize(r['elements'])
     return players
+
+
+def gameweekHistory(element_id):
+    url = f'https://fantasy.premierleague.com/api/element-summary/{element_id}/'
+    r = requests.get(url)
+    json = r.json()
+    print(json.keys())
+    json_history_df = pd.DataFrame(json['history'])
+    print(json_history_df.head())
+
+
+# gameweekHistory(200)
+
+
+def get_gameweek_history(player_id):
+    '''get all gameweek info for a given player_id'''
+
+    # send GET request to
+    # https://fantasy.premierleague.com/api/element-summary/{PID}/
+    r = requests.get(
+        base_url + 'element-summary/' + str(player_id) + '/'
+    ).json()
+
+    # extract 'history' data from response into dataframe
+    # print(r)
+    df = pd.json_normalize(r['history'])
+    # print(df.head())
+    df = df[[
+        'round',
+        'total_points',
+        'minutes',
+        'goals_scored',
+        'assists',
+        'clean_sheets',
+        'saves',
+        'bonus',
+        'value',
+        'element'
+    ]]
+    return df
+
+
+"""def apitest():
+    link = "https://understat.com/player/5232"
+    r = requests.get(link).json()
+    print(r)
+
+
+apitest()"""
+# print(get_gameweek_history(200))
--- dash_fpl/dataprep.py
+++ dash_fpl/dataprep.py
@@ -8,15 +8,18 @@ import plotly.express as px
 import pandas as pd
 from PIL import Image
 import dash_bootstrap_components as dbc
-from data_extraction import getPlayer
+from data_extraction import getPlayer, get_gameweek_history
 
-pyLogo = Image.open("/Users/brendanbaker/DashFPL/Screenshot 2022-02-25 at 22.09.49.png")
+pyLogo = Image.open("/Users/brendanbaker/DashFPL/Screenshot 2022-03-01 at 19.12.20.png")
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
 pd.set_option('display.max_columns', 500)
+
+
+
 
 df = pd.read_csv('/Users/brendanbaker/PycharmProjects/UnderstatAPI/Testing/shot_data.csv')
 pdff = getPlayer()
@@ -30,11 +33,17 @@ df['Y'] = ((df['Y']) * 100)
 # print(df.iloc[[5042]])
 # df = df.iloc[[1583]]
 # del pdf['dreamteam_count'], pdf['special'], pdf['squad_number'], pdf['bps'], pdf['influence'], pdf['creativity'], pdf['threat'], pdf['ict_index'], pdf['influence_rank'], pdf['influence_rank_type'], pdf['creativity_rank']
-pdf = pdff[['web_name', 'status', 'total_points', 'goals_scored', 'assists', 'minutes', 'bonus']]
+pdf = pdff[['web_name', 'status', 'total_points', 'goals_scored', 'assists', 'minutes', 'bonus', 'selected_by_percent',
+            'now_cost', 'team', 'news', 'id']]
+
+pdf["selected_by_percent"] = pd.to_numeric(pdf["selected_by_percent"], downcast="float")
+
+print(type(pdf['selected_by_percent'][0]))
+
+final_pdf = pdf.sort_values(by=['selected_by_percent'], ascending=False)
 all = df['result'].unique()
 options = [{'label': x, 'value': x} for x in all]
 options.append({'label': 'Select All', 'value': "all"})
-
 app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(html.H1("FPL Dashboard",
@@ -43,14 +52,20 @@ app.layout = dbc.Container([
                 width=12)
     ]),
     dbc.Row([
-        dbc.Col(dcc.Dropdown(df['player'].unique(), id='player',),
+        dbc.Col(dcc.Dropdown(df['player'].unique(), id='player', ),
                 width={'size': 2, "offset": 1, 'order': 0}
                 ),
 
         dbc.Col(dcc.Dropdown(id='choice', value='all_values',
                              options=[{'label': 'Select all', 'value': 'all_values'}] + [{'label': x, 'value': x} for x
                                                                                          in all]),
-                width={'size': 2, "offset": 0, 'order': 1}
+                width={'size': 2, "offset": 1, 'order': 1}
+                ),
+        dbc.Col(dcc.Dropdown(final_pdf['web_name'].unique(), id='player1', value='Ziyech'),
+                width={'size': 2, "offset": 0, 'order': 2}
+                ),
+        dbc.Col(dcc.Dropdown(final_pdf['web_name'].unique(), id='player2', value='Ronaldo'),
+                width={'size': 2, "offset": 1, 'order': 2}
                 ),
 
     ]),
@@ -59,6 +74,8 @@ app.layout = dbc.Container([
         dbc.Col(dcc.Graph(id='shot-map', figure={}, config={'displaylogo': False, 'displayModeBar': False}),
                 width=6, lg={'size': 5, "offset": 1, 'order': 'first'}
                 ),
+        dbc.Col(dcc.Graph(id='lc', figure={}, config={'displaylogo': False, 'displayModeBar': False}),
+                width=6, lg={'size': 6, "offset": 0, 'order': 'second'}),
 
     ]),
 
@@ -74,15 +91,54 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([
             dash_table.DataTable(
-                pdf.to_dict('records'),
-                [{"name": i, "id": i} for i in pdf.columns],
-                page_size=10,
-                style_table={'overflowX': 'auto'},
-                fill_width=False
+                final_pdf.to_dict('records'),
+                [{"name": i, "id": i} for i in final_pdf.columns],
+                filter_action='native',
+                page_size=5,
+                style_table={'overflowX': 'auto', 'height': '250px'},
+                fill_width=False,
+                style_data={'whiteSpace': 'normal', 'height': 'auto', 'font_size': 15},
+                style_cell={
+                    # 'height': 'auto',
+                    # all three widths are needed
+                    # 'minWidth': '100px', 'width': '100px', 'maxWidth': '100px',
+                    # 'minWidth': '180px', 'width': '180px', 'maxWidth': '180px',
+                    'whiteSpace': 'normal'
+                },
+                style_cell_conditional=[
+                    {'if': {'column_id': 'news'},
+                     'width': '300px'},
+                    {'if': {'column_id': 'web_name'},
+                     'width': '150px'}],
             ),
-        ]),
+        ], width={'size': 10, "offset": 1, 'order': 0}),
     ])
 ], fluid=True)
+
+@app.callback(
+    Output('lc', 'figure'),
+    Input('player1', 'value'),
+    Input('player2', 'value'),
+)
+def lineChart(player1, player2):
+    df1 = final_pdf[final_pdf['web_name'] == player1]
+    df2 = final_pdf[final_pdf['web_name'] == player2]
+    #print(dff)
+    id_1 = df1['id'].iloc[0]
+    id_2 = df2['id'].iloc[0]
+    int_id_1 = int(id_1)
+    int_id_2 = int(id_2)
+
+    gwdf1 = get_gameweek_history(int_id_1)
+    gwdf2 = get_gameweek_history(int_id_2)
+
+    gwdf1.loc[gwdf1['minutes'] == 0, 'total_points'] = None
+    gwdf2.loc[gwdf2['minutes'] == 0, 'total_points'] = None
+
+    fig = px.line(gwdf1, x='round', y='total_points', title="Gameweek Payer Data", hover_data=['minutes'])
+    fig.update_traces(connectgaps=False)
+
+    return fig
 
 
 @app.callback(
@@ -99,10 +155,11 @@ def update_graph(player, choice):
     else:
         dff = dff[dff['result'] == choice]
 
-    fig = px.scatter(dff, x="X", y="Y", color='result', size='xG', size_max=25, hover_name="player", hover_data=[
-        "result", "situation", 'player_assisted'])
+    fig = px.scatter(dff, x="X", y="Y", color='result', size_max=25, hover_name="player",
+                     hover_data={"result": True, "situation": True, 'player_assisted': True, 'X': False, 'Y': False}
+                     , size='xG')
 
-    fig.update_layout(margin={'l': 0, 'b': 0, 't': 25, 'r': 0}, hovermode='closest', )
+    fig.update_layout(margin={'l': 0, 'b': 0, 't': 20, 'r': 0}, hovermode='closest',)
 
     # fig.update_layout(template="simple_white", width=1070, height=711,
     # xaxis_showgrid=False, yaxis_showgrid=False)
