1de21a4dbb46724865f967b54776e6aa10410073
Brendan <brendanpatrickbaker7@gmail.com>
2022-04-23 02:14:45 +0100

    Additional Unit Tests

--- dash_fpl/captain_picks.py
+++ dash_fpl/captain_picks.py
@@ -8,11 +8,12 @@ pd.set_option('display.max_columns', None)
 
 # https://fantasy.premierleague.com/api/dream-team/8/ ----> highest scoring
 
+# TODO
 def multiply_by_2(dataframe):
     captain = dataframe.loc[dataframe['multiplier'] == 2]
     return captain
 
-
+# TODO
 def multiply_by_3(dataframe):
     captain = dataframe.loc[dataframe['multiplier'] == 3]
     return captain
@@ -34,10 +35,12 @@ def points_multiplier(dataframe):
     return dataframe.points * 2
 
 
+# TESTED
 def captain_points_difference(dataframe):
     return dataframe['Points'] - dataframe['Optimal Points']
 
 
+# TESTED
 def get_GW_captain_picks(manager_id, gameweek, df):
     while 1:
         r = requests.get(f"https://fantasy.premierleague.com/api/entry/{manager_id}/event/{gameweek}/picks/")
@@ -48,19 +51,22 @@ def get_GW_captain_picks(manager_id, gameweek, df):
             data = (json.loads(r.text))
             dataset = pd.DataFrame.from_dict(data['picks'])
             if dataset['multiplier'].eq(2).any():
-                cpt = dataset.loc[dataset['multiplier'] == 2]
+                cpt = multiply_by_2(dataset)
             elif dataset['multiplier'].eq(3).any():
-                cpt = dataset.loc[dataset['multiplier'] == 3]
+                cpt = multiply_by_3(dataset)
             df = pd.concat([df, cpt])
         gameweek += 1
 
-
+# TESTED
 def match_captain_pick_to_score(captains, captain_points, gameweek):
     for i in captains:
 
         r5 = requests.get(f"https://fantasy.premierleague.com/api/element-summary/{i}/")
         test = (json.loads(r5.text))
-        elements_df = pd.DataFrame(test['history'])
+        try:
+            elements_df = pd.DataFrame(test['history'])
+        except KeyError:
+            return captain_points, gameweek
         elements_df = elements_df[['total_points', 'round']]
         aggregation_functions = {'total_points': 'sum', 'round': 'sum'}
         df_new = elements_df.groupby(elements_df['round']).aggregate(aggregation_functions)
@@ -75,7 +81,7 @@ def match_captain_pick_to_score(captains, captain_points, gameweek):
             gameweek = 1
             return captain_points, gameweek
 
-
+# TODO
 def get_GW_optimal_captains(topPlayerDf, gameweek):
     while 1:
         top_player_request = requests.get(f"https://fantasy.premierleague.com/api/dream-team/{gameweek}/")
@@ -88,7 +94,7 @@ def get_GW_optimal_captains(topPlayerDf, gameweek):
             topPlayerDf = pd.concat([topPlayerDf, temp_dataset])
             gameweek += 1
 
-
+# TODO
 def getGenericPlayerData():
     requestGenericPlayerData = requests.get("https://fantasy.premierleague.com/api/bootstrap-static/")
     genericPLayerData = requestGenericPlayerData.json()
@@ -96,18 +102,18 @@ def getGenericPlayerData():
     playerElements = playerElementsTemp[['id', 'web_name']]
     return playerElements
 
-
+# TODO
 # List to series to col
 def captain_points_to_col(captain_points):
     return pd.Series(captain_points)
 
-
+# TODO
 def dataframe_col_rename(dataframe):
     dataframe = dataframe.rename(
         columns={'element': 'Captain', 'total_points': 'Points', 'id': 'Optimal Pick', 'points': 'Optimal Points'})
     return dataframe
 
-
+# TODO
 # Test input a variable, expect a dataframe
 def optimal_captain(manager_id):
     captain_points = []
--- test/unit_tests.py
+++ test/unit_tests.py
@@ -1,5 +1,9 @@
 import unittest
-from dash_fpl.captain_picks import total_points_multiplier, dataframe_col_to_list, points_multiplier, captain_points_difference
+
+import pandas
+from dash_fpl.captain_picks import total_points_multiplier, dataframe_col_to_list, points_multiplier, \
+    captain_points_difference, get_GW_captain_picks, match_captain_pick_to_score
+
 from pandas.testing import assert_frame_equal, assert_series_equal
 import pandas as pd
 
@@ -68,13 +72,92 @@ class TestPointsMultiplierForOptimalCaptains(unittest.TestCase):
 
 class TestTheDifferenceBetweenManagersCaptainAndOptimal(unittest.TestCase):
 
+    def test_difference_between_points_data_struct(self):
+        df = pd.DataFrame({
+            'Points': [2, -2, 16, 2],
+            'Optimal Points': [10, 7, 16, 7]
+        })
+        expected = pd.Series([-8, -9, 0, -5])
+        actual = captain_points_difference(df)
+        self.assertEqual(type(expected), type(actual))
+
     def test_difference_between_points(self):
         df = pd.DataFrame({
-            'Points': [2, -2, 16, 7],
-            'Optimal Points': [10, 7, 2, 2]
+            'Points': [2, -2, 16, 2],
+            'Optimal Points': [10, 7, 16, 7]
         })
-        expected = pd.Series([8, 5, 14, 4])
+        expected = pd.Series([-8, -9, 0, -5])
         actual = captain_points_difference(df)
+        pd.testing.assert_series_equal(expected, actual)
+
+
+# valid manager id and gameweek
+class TestGameWeekCaptainPicksOfManager(unittest.TestCase):
+
+    def test_invalid_manager_id(self):
+        expectedDF = pd.DataFrame()
+        expectedGameweek = 1
+        managerID = 'asdr123'
+        actualDF, actualGameWeek = get_GW_captain_picks(managerID, expectedGameweek, expectedDF)
+        self.assertEqual(expectedGameweek, actualGameWeek)
+        pd.testing.assert_frame_equal(expectedDF, actualDF)
+
+    def test_invalid_gameweek(self):
+        expectedDF = pd.DataFrame()
+        invalidGameweekInput = 100
+        expectedGameweek = 1
+        managerID = '1'
+        actualDF, actualGameWeek = get_GW_captain_picks(managerID, invalidGameweekInput, expectedDF)
+        self.assertEqual(expectedGameweek, actualGameWeek)
+        pd.testing.assert_frame_equal(expectedDF, actualDF)
+
+    def test_data_types_and_structures(self):
+        expectedDF = pd.DataFrame()
+        expectedGameweek = 1
+        managerID = '1'
+        actualDF, actualGameWeek = get_GW_captain_picks(managerID, expectedGameweek, expectedDF)
+        self.assertEqual(type(expectedGameweek), type(actualGameWeek))
+        self.assertEqual(type(expectedDF), type(actualDF))
+
+
+class TestFindScoresForManagerCaptainPicksEachWeek(unittest.TestCase):
+
+    def test_captain_gameweek_points_match(self):
+        captains = [233]
+        captain_points = []
+        expectedCaptainPoints = [17]
+        expectedGameweek = 1
+
+        actualCaptainPoints, actualGameWeek = match_captain_pick_to_score(captains, captain_points, expectedGameweek)
+        self.assertEqual(expectedGameweek, actualGameWeek)
+        self.assertEqual(expectedCaptainPoints, actualCaptainPoints)
+
+    def test_if_no_captain_TypeError(self):
+        captains = []
+        captain_points = []
+        #expectedCaptainPoints =
+        expectedGameweek = 1
+        self.assertRaises(TypeError, match_captain_pick_to_score(captains, captain_points, expectedGameweek))
+
+    def test_if_invalid_captain_returns_empty_list(self):
+        captains = [1000]
+        captain_points = []
+        expectedCaptainPoints = []
+        expectedGameweek = 1
+        actualCaptainPoints, actualGameWeek = match_captain_pick_to_score(captains, captain_points, expectedGameweek)
+        self.assertEqual(expectedCaptainPoints, actualCaptainPoints)
+
+    def test_data_types(self):
+        captains = [233]
+        captain_points = []
+        expectedCaptainPoints = [17]
+        expectedGameweek = 1
+
+        actualCaptainPoints, actualGameWeek = match_captain_pick_to_score(captains, captain_points, expectedGameweek)
+        self.assertEqual(type(expectedGameweek), type(actualGameWeek))
+        self.assertEqual(type(expectedCaptainPoints), type(actualCaptainPoints))
+
+
 
 if __name__ == '__main__':
     unittest.main()
